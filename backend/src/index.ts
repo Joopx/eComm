3a5a7d6506fcb23a8e927f50f79db8cb6b398daf
Joopx <jyotimandalw2712@gmail.com>
2026-07-22 22:38:35 +0530

    feat: update package.json and package-lock.json for backend and frontend, add e_comm dependency, and enhance build/start scripts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import {ENV} from "./config/env"
+import path from "path"
 import {clerkMiddleware} from "@clerk/express";
 import cors from "cors";
 import { User } from "./db/schema";
 import userRoutes from "./routes/userRoutes";
 import productRoutes from "./routes/productRoutes";
 import commentRoutes from "./routes//commentRoutes";
-
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(clerkMiddleware()); // auth obj will be attched to the req
 app.use(express.json()); //parses JSON req bodies
 app.use(express.urlencoded({extended:true})); // parses form data(html forms)
 
-app.get("/", (req,res)=>{
+app.get("/api/health", (req,res)=>{
 
      
     res.json({
@@ -35,5 +35,16 @@ app.use("/api/users",userRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/comments",commentRoutes);
 
+
+if(ENV.NODE_ENV === "production"){
+    const __dirname = path.resolve();
+    //serve static files from frontend/dist
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    //handle SPA routing - send all non API routes to indexhtml - react app
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend/dist/index"))
+    });
+}
 app.listen(ENV.PORT,()=>
     console.log(`Listening on Port:${ENV.PORT}`));
